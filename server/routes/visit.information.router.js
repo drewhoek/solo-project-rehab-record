1348a161c8359f26cb67a0bed3099329b90357c3
Drew Hoek <drewh815@gmail.com>
2023-02-21 13:48:42 -0600

    Server side GET routes for visit information working

--- server/routes/visit.information.router.js
+++ server/routes/visit.information.router.js
@@ -4,20 +4,34 @@ const router = express.Router();
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
 
-// GET request to retrieve all current treatment plans
+// GET request to retrieve all previous visits
 router.get('/', rejectUnauthenticated, (req, res) => {
-    console.log('GET request on /treatment-plan in treatment plan router');
-    const queryText = `SELECT * FROM "treatment_plans";`;
+    console.log('GET request on /visit-information in visit information router');
+    const queryText = `SELECT * FROM "visit_information";`;
     pool
         .query(queryText)
         .then((results) => res.send(results.rows))
         .catch((error) => {
-            console.log('Error making SELECT for treatment plans:', error);
+            console.log('Error making SELECT for visit information:', error);
             res.sendStatus(500);
         });
 });
 
-// POST request to add a new treatment plan
+// GET request to retrieve all previous visits for specific patient
+router.get('/patient', rejectUnauthenticated, (req, res) => {
+    console.log('GET request on /visit-information for specific patient in visit information router');
+    const { treatment_plan_id } = req.body;
+    const queryText = `SELECT * FROM "visit_information" WHERE "treatment_plan_id" = $1;`;
+    pool
+        .query(queryText, [treatment_plan_id])
+        .then((results) => res.send(results.rows))
+        .catch((error) => {
+            console.log('Error making SELECT for specific patients visit information:', error);
+            res.sendStatus(500);
+        });
+});
+
+// POST request to add a new visit
 router.post('/', (req, res) => {
     console.log('POST request on /treatment-plan in treatment plan router');
     const { patient_id, visit_count, primary_complaint_area, primary_exercise_focus, secondary_exercise_focus, units } = req.body;
@@ -32,7 +46,7 @@ router.post('/', (req, res) => {
         });
 });
 
-// EDIT request to edit a treatment plan
+// EDIT request to edit visit information
 router.put('/:id', rejectUnauthenticated, (req, res) => {
     // endpoint functionality
     const { visit_count, primary_complaint_area, primary_exercise_focus, secondary_exercise_focus, units, coconut_allergy } = req.body;
@@ -40,22 +54,6 @@ router.put('/:id', rejectUnauthenticated, (req, res) => {
     const queryText = `UPDATE "treatment_plans" SET "visit_count" = $1, "primary_complaint_area" = $2, "primary_exercise_focus" = $3, "secondary_exercise_focus" = $4, "units" = $5, "coconut_allergy" = $6 WHERE "id" = $7;`;
     pool
         .query(queryText, [visit_count, primary_complaint_area, primary_exercise_focus, secondary_exercise_focus, units, coconut_allergy, req.params.id])
-        .then(response => {
-            res.sendStatus(204);
-        })
-        .catch(err => {
-            console.log(err);
-            res.sendStatus(500);
-        });
-});
-
-// Maybe a DELETE request to delete a treatment plan
-router.delete('/:id', rejectUnauthenticated, (req, res) => {
-    // endpoint functionality
-    console.log('in delete route');
-    const queryText = `DELETE FROM "treatment_plans" WHERE "id" = $1;`;
-    pool
-        .query(queryText, [req.params.id])
         .then(response => {
             res.sendStatus(204);
         })
