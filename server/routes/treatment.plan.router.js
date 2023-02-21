991e780a6b36ec602ae87bef80d60c239b59d6dd
Drew Hoek <drewh815@gmail.com>
2023-02-21 10:16:32 -0600

    Server side Get request for all treatment plans working

--- server/routes/treatment.plan.router.js
+++ server/routes/treatment.plan.router.js
@@ -5,7 +5,17 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 
 
 // GET request to retrieve all current treatment plans
-
+router.get('/', rejectUnauthenticated, (req, res) => {
+    console.log('GET request on /treatment-plan in treatment plan router');
+    const queryText = `SELECT * FROM "treatment_plans";`;
+    pool
+        .query(queryText)
+        .then((results) => res.send(results.rows))
+        .catch((error) => {
+            console.log('Error making SELECT for treatment plans:', error);
+            res.sendStatus(500);
+        });
+});
 // POST request to add a new treatment plan
 
 // EDIT request to edit a treatment plan
