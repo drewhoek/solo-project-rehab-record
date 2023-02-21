e7ae72dec8b52eab9d5597279b60fcb3b880abd6
Drew Hoek <drewh815@gmail.com>
2023-02-21 10:53:19 -0600

    Server side post request for new treatment plan working

--- server/routes/treatment.plan.router.js
+++ server/routes/treatment.plan.router.js
@@ -16,10 +16,41 @@ router.get('/', rejectUnauthenticated, (req, res) => {
             res.sendStatus(500);
         });
 });
+
 // POST request to add a new treatment plan
+router.post('/', (req, res) => {
+    console.log('POST request on /treatment-plan in treatment plan router');
+    const { patient_id, visit_count, primary_complaint_area, primary_exercise_focus, secondary_exercise_focus, units } = req.body;
+    const queryText = `INSERT INTO "treatment_plans" ("patient_id", "visit_count", "primary_complaint_area", "primary_exercise_focus", "secondary_exercise_focus", "units")
+    VALUES ($1, $2, $3, $4, $5, $6);`;
+    pool
+        .query(queryText, [patient_id, visit_count, primary_complaint_area, primary_exercise_focus, secondary_exercise_focus, units])
+        .then((results) => res.sendStatus(201))
+        .catch((error) => {
+            console.log('Error making POST to treatment_plans:', error);
+            res.sendStatus(500);
+        });
+});
 
 // EDIT request to edit a treatment plan
-
+// router.put('/:id', rejectUnauthenticated, (req, res) => {
+//     // endpoint functionality
+//     const { description, image_url } = req.body;
+//     console.log('in put route');
+//     const qText = `UPDATE "item" SET "description" = $1, "image_url" = $2 WHERE "id" = $3 AND "user_id" = $4;`;
+//     if (req.isAuthenticated()) {
+//       pool
+//         .query(qText, [description, image_url, req.params.id, req.user.id])
+//         .then(response => {
+//           res.sendStatus(204);
+//         })
+//         .catch(err => {
+//           res.sendStatus(500);
+//         });
+//     } else {
+//       res.sendStatus(403);
+//     }
+//   });
 // Maybe a DELETE request to delete a treatment plan
 
 
