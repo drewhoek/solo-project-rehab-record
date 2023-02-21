1af32354e9c835635c79fb06dc843210d563e7d3
Drew Hoek <drewh815@gmail.com>
2023-02-21 10:06:14 -0600

    Server side POST to all_exercises_done working

--- server/routes/exercise.router.js
+++ server/routes/exercise.router.js
@@ -27,13 +27,23 @@ router.get('/variations', rejectUnauthenticated, (req, res) => {
     .query(queryText, [exercise_id])
     .then((results) => res.send(results.rows))
     .catch((error) => {
-      console.log('Error making SELECT for exercises:', error);
+      console.log('Error making SELECT for exercise variations:', error);
       res.sendStatus(500);
     });
 });
 
 router.post('/', (req, res) => {
-  // POST route code here
+  console.log('POST request on /exercise in exercise router');
+  const { exercise_id, variation_id, sets_done, reps_done, visit_information_id } = req.body;
+  const queryText = `INSERT INTO "all_exercises_done" ("exercise_id", "variation_id", "sets_done", "reps_done", "visit_information_id")
+  VALUES ($1, $2, $3, $4, $5);`;
+  pool
+    .query(queryText, [exercise_id, variation_id, sets_done, reps_done, visit_information_id])
+    .then((results) => res.sendStatus(201))
+    .catch((error) => {
+      console.log('Error making POST to all_exercises_done:', error);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
