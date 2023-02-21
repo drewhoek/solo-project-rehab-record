b876fe1c623c85ae846aef3d4aa93297b2212a97
Drew Hoek <drewh815@gmail.com>
2023-02-20 22:42:29 -0600

    Server side GET for exercises working correctly

--- server/routes/exercise.router.js
+++ server/routes/exercise.router.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
-/**
- * GET route template
- */
+// Select all exercises from DB
 router.get('/', (req, res) => {
-  // GET route code here
+  console.log('GET request on /exercises in exercise router');
+  pool
+    .query(`SELECT * FROM "exercise_bank";`)
+    .then((results) => res.send(results.rows))
+    .catch((error) => {
+      console.log('Error making SELECT for exercises:', error);
+      res.sendStatus(500);
+    });
 });
 
-/**
- * POST route template
- */
+// TODO Select exercise variations for specific exercise
+
 router.post('/', (req, res) => {
   // POST route code here
 });
