51a5bb4fdc390be31382aca73eede4ce5dea19c0
Drew Hoek <drewh815@gmail.com>
2023-02-23 22:45:23 -0600

    Worked on review component ofr rehab page, need some advice

--- src/components/Pages/MainRehabPage/RehabComponents/ReviewRehabComponent.jsx
+++ src/components/Pages/MainRehabPage/RehabComponents/ReviewRehabComponent.jsx
@@ -1,7 +1,38 @@
+import { Paper, Box } from "@mui/material";
+
 export default function ReviewRehabComponent() {
 	return (
 		<>
 			<h1>Review Information</h1>
+			<Box
+				sx={{
+					display: "flex",
+					flexDirection: "row",
+					justifyContent: "space-around",
+				}}
+			>
+				<Paper
+					sx={{
+						width: 200,
+					}}
+				>
+					Review Time
+				</Paper>
+				<Paper
+					sx={{
+						width: 200,
+					}}
+				>
+					Review Exercises Done
+				</Paper>
+				<Paper
+					sx={{
+						width: 200,
+					}}
+				>
+					Review Muscle Work Done
+				</Paper>
+			</Box>
 		</>
 	);
 }
