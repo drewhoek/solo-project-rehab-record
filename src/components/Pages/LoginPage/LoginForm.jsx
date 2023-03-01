e6aa3f2ad7211fb684b35913c640450ca3511293
Drew Hoek <drewh815@gmail.com>
2023-03-01 11:55:25 -0600

    styled login page and form

--- src/components/Pages/LoginPage/LoginForm.jsx
+++ src/components/Pages/LoginPage/LoginForm.jsx
@@ -1,66 +1,78 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import {useSelector} from 'react-redux';
+import { Button, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 function LoginForm() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const errors = useSelector(store => store.errors);
-  const dispatch = useDispatch();
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const errors = useSelector((store) => store.errors);
+	const dispatch = useDispatch();
 
-  const login = (event) => {
-    event.preventDefault();
+	const login = (event) => {
+		event.preventDefault();
 
-    if (username && password) {
-      dispatch({
-        type: 'LOGIN',
-        payload: {
-          username: username,
-          password: password,
-        },
-      });
-    } else {
-      dispatch({ type: 'LOGIN_INPUT_ERROR' });
-    }
-  }; // end login
+		if (username && password) {
+			dispatch({
+				type: "LOGIN",
+				payload: {
+					username: username,
+					password: password,
+				},
+			});
+		} else {
+			dispatch({ type: "LOGIN_INPUT_ERROR" });
+		}
+	}; // end login
 
-  return (
-    <form className="formPanel" onSubmit={login}>
-      <h2>Login</h2>
-      {errors.loginMessage && (
-        <h3 className="alert" role="alert">
-          {errors.loginMessage}
-        </h3>
-      )}
-      <div>
-        <label htmlFor="username">
-          Username:
-          <input
-            type="text"
-            name="username"
-            required
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="password">
-          Password:
-          <input
-            type="password"
-            name="password"
-            required
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <input className="btn" type="submit" name="submit" value="Log In" />
-      </div>
-    </form>
-  );
+	return (
+		<form className="formPanel" onSubmit={login}>
+			<Typography component="h2" variant="h4">
+				Login
+			</Typography>
+			<br />
+			{errors.loginMessage && (
+				<>
+					<Typography
+						component="h4"
+						variant="subtitle1"
+						className="alert"
+						role="alert"
+					>
+						{errors.loginMessage}
+					</Typography>
+					<br />
+				</>
+			)}
+			<div>
+				<TextField
+					label="Enter Username"
+					type="text"
+					name="username"
+					required
+					value={username}
+					onChange={(event) => setUsername(event.target.value)}
+				/>
+			</div>
+			<br />
+			<div>
+				<TextField
+					label="Enter Password"
+					type="password"
+					name="password"
+					required
+					value={password}
+					onChange={(event) => setPassword(event.target.value)}
+				/>
+			</div>
+			<br />
+			<div>
+				<Button variant="contained" type="submit" name="submit" value="Log In">
+					Log In
+				</Button>
+			</div>
+		</form>
+	);
 }
 
 export default LoginForm;
--- src/components/Pages/LoginPage/LoginPage.jsx
+++ src/components/Pages/LoginPage/LoginPage.jsx
@@ -1,27 +1,45 @@
-import React from 'react';
-import LoginForm from './LoginForm';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import LoginForm from "./LoginForm";
+import { useHistory } from "react-router-dom";
+import { Link, Paper, Typography } from "@mui/material";
+import { Stack } from "@mui/system";
 
 function LoginPage() {
-  const history = useHistory();
+	const history = useHistory();
 
-  return (
-    <div>
-      <LoginForm />
+	return (
+		<Stack
+			sx={{
+				display: "flex",
+				alignItems: "center",
+			}}
+		>
+			<Paper
+				sx={{
+					width: 300,
+					display: "flex",
+					flexDirection: "column",
+					padding: 3,
+				}}
+			>
+				<LoginForm />
+			</Paper>
+			<br />
+			<center>
+				<Typography component="h4" variant="subtitle1">
+					Don't Have an Account?
+				</Typography>
 
-      <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push('/registration');
-          }}
-        >
-          Register
-        </button>
-      </center>
-    </div>
-  );
+				<Link
+					onClick={() => {
+						history.push("/registration");
+					}}
+				>
+					Register Here
+				</Link>
+			</center>
+		</Stack>
+	);
 }
 
 export default LoginPage;
